2002f57bc6d9e3ee1658b98ab06e5794a11a1632
NSDeveloper <hnldgzl@gmail.com>
2016-09-22 14:12:57 +0800

    Imported user code snippets

--- objective-c_property.m
+++ objective-c_property.m
@@ -3,6 +3,7 @@
 //
 // IDECodeSnippetCompletionPrefix: @pt
 // IDECodeSnippetCompletionScopes: [ClassInterfaceMethods]
+// IDECodeSnippetIdentifier: BEB39BD6-5E73-4FAD-9F35-8E261322780C
 // IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
 // IDECodeSnippetUserSnippet: 1
 // IDECodeSnippetVersion: 1
